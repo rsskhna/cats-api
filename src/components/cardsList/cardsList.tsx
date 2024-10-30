0fb65ed47d3194cc107fd826d238054b08a96aa9
Irina Rossokhina <i.rossokhina@yandex.ru>
2024-10-30 17:50:47 +0300

    nanoid

--- src/components/cardsList/cardsList.tsx
+++ src/components/cardsList/cardsList.tsx
@@ -2,6 +2,7 @@ import styles from './cardsList.module.css';
 import Card from '../card/card';
 import { TCat } from '../../utils/types';
 import React, { FC } from 'react';
+import { nanoid } from '@reduxjs/toolkit';
 
 type TCardsListComponent = {
   cards: TCat[];
@@ -10,7 +11,7 @@ type TCardsListComponent = {
 const cardsList: FC<TCardsListComponent> = ({ cards }) => (
   <ul className={styles.list}>
     {cards.map((card, index) => (
-      <Card imageUrl={card.url} key={card.id} cardIndex={index} />
+      <Card imageUrl={card.url} key={nanoid()} cardIndex={index} />
     ))}
   </ul>
 );
